8992308fa6d11b314b78e36e8e42718ca9b92ea3
Thomas Jacobsen <thomasj4332@gmail.com>
2020-03-24 22:14:49 -0600

    Initial commit

--- src/CardTester.java
+++ src/CardTester.java
@@ -9,5 +9,27 @@ public class CardTester {
 	 */
 	public static void main(String[] args) {
 		/* *** TO BE IMPLEMENTED IN ACTIVITY 1 *** */
+
+		Card card1 = new Card("4", "Diamond", 3);
+		Card card2 = new Card("2", "Spades", 6);
+		Card card3 = new Card("4", "Diamond", 3);
+
+		if (card1.matches(card2)) {
+			System.out.println("One matches two");
+		} else {
+			System.out.println("One does not match two");
+		}
+		
+		if (card1.matches(card3)) {
+			System.out.println("One matches three");
+		} else {
+			System.out.println("One does not match three");
+		}
+
+		if (card2.matches(card3)) {
+			System.out.println("Two matches three");
+		} else {
+			System.out.println("Two does not match three");
+		}
 	}
 }
